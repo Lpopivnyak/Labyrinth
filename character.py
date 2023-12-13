5bd7ed27b28dc0985f0904619b6f028d6bf544fb
sauceTartarys <136169348+lpopivnyak@users.noreply.github.com>
2023-12-13 17:24:04 +0200

    Escape3

--- character.py
+++ character.py
@@ -4,10 +4,12 @@ class Character:
     def __init__(self, x, y, w, h, speed, texture):
         self.speed = speed
         self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, (w, h))
         self.hitbox = self.texture.get_rect()
         self.hitbox.x = x
         self.hitbox.y = y
-        self.texture = pygame.transform.scale(self.texture, (w, h))
+
+
 
     def Render(self, window):
         window.blit(self.texture, (self.hitbox.x, self.hitbox.y))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import character
 import enemy
 import gold
+
 import wall
 
 pygame.init()
@@ -12,6 +13,7 @@ fps = pygame.time.Clock()
 pygame.mixer.init()
 pygame.mixer.music.load("jungles.ogg")
 pygame.mixer.music.play(-1)
+money_taunt = pygame.mixer.Sound("money.ogg")
 
 backgroundImage = pygame.image.load("background.jpg")
 backgroundImage = pygame.transform.scale(backgroundImage, (1370, 705))
@@ -38,6 +40,14 @@ while game:
 
     #оновлення
     pacman_Ghost.movement()
+
+    for wall in walls:
+        if pacman_Ghost.hitbox.colliderect(wall.rect):
+            game = False
+
+    if pacman_Ghost.hitbox.colliderect(gold.hitbox):
+        money_taunt.play()
+        gold.hitbox.x = 1000000
 
     #рендер
     window.fill((0, 0, 0))
